c904d8350406cca3e036f5a73763a93585c58b3c
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-12-12 14:51:11 +0530

    Add files via upload

---
+++ 03_slicingstrings.py
@@ -0,0 +1,6 @@
+b="hello world!"
+print(b[2:5])
+print(b[:5])
+# Note: The first character has index 0.
+print(b[5:])
+print(b[-5:-2])
---
+++ 06_stringformat.py
@@ -0,0 +1,9 @@
+age=19
+txt="my name is siddhi , i am  {}"
+print(txt.format(age))
+
+quantity=3
+itemo=587
+price=23.3
+myorder="i want {} pieeces of item{} for {} euro"
+print(myorder.format(quantity,itemo,price))
