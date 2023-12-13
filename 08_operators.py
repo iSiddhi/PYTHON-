81a5994f35a072c121b1af92940f6775aea8c2cf
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-12-13 17:47:50 +0530

    Add files via upload

---
+++ 08_operators.py
@@ -0,0 +1,8 @@
+print(10+5)
+x=5
+
+x>>=3
+print(x)
+
+x<<=3
+print(x)
---
+++ 09_list.py
@@ -0,0 +1,32 @@
+myList=["apple","pineapple","banana"]
+# print(myList)
+# print(len(myList))
+# print(type(myList))
+# print(myList[1])
+# print(myList[1:2])
+# print(myList[:2])
+# print(myList[1:])
+# if "apple " in myList:
+#     print("yes 'apple' is in the fruits")
+
+# myList[1]="blackcurrant"
+# print(myList)
+
+# myList[2]="mango"
+# print(myList)
+# myList[0:2] = [ "siddhi" , "watermelon"]
+# print(myList)
+
+# myList.insert(2,"watermelon")
+# print(myList)
+
+# myList.insert(1,"orange")
+# print(myList)
+
+# tropical=["mango","pineapple","papaya"]
+# myList.extend(tropical)
+# print(myList)
+
+# myList.pop(1)
+# print(myList)
+
