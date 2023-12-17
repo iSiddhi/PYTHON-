aa7d7dd49db79556ad1b4bb90738178cabf8e29c
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-12-18 00:14:52 +0530

    Add files via upload

---
+++ 18_classses.py
@@ -0,0 +1,33 @@
+# class Employee:
+#     x=5
+
+# e1=Employee()
+# print(e1.x)
+
+'''class myclass:
+    x=8
+
+s1=myclass()
+print(s1.x)
+
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+p1=Person("john",36)
+print(p1.name)
+print(p1.age)  '''      
+        
+
+class Student:
+    def __init__(self,name,classss,roll) :
+        self.name=name
+        self.roll=roll
+        self.classss=classss
+s1=Student("siddji",2,34)
+print(s1.name)
+print(s1.classss)                
+print(s1.roll)                                
