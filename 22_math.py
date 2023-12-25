c4301b5cea123e73e0a9f5900096d13baded4b9c
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-12-25 20:17:42 +0530

    Add files via upload

---
+++ 22_math.py
@@ -0,0 +1,25 @@
+
+
+# x=min(5,10,25)
+# y=max(5,10,25)
+# print(x)
+# print(y)
+
+
+#  x=abs(-7.25)
+#  print(x)
+
+# x=pow(4,3)
+# print(x)
+
+
+import math
+x=math.sqrt(64)
+print(x)
+
+y=math.sqrt(144)
+print(y)
+
+
+z=math.sqrt(196)
+print(z)
---
+++ 23_math.py
@@ -0,0 +1,13 @@
+# import math
+# x=math.ceil(1.4)
+# y=math.ceil(1.1)
+# print(x)
+# print(y)
+
+# x=math.floor(1.4)
+# print(x)
+
+
+import math 
+x=math.pi
+print(x)
